dd9bdb1acf1cfe2fd612ae9cb6c08990176e573b
stemberk <stemberk@spsmb.cz>
2022-09-20 14:39:30 +0200

    _

--- w02/print_unicode.py
+++ w02/print_unicode.py
@@ -23,7 +23,7 @@ def print_unicode_table(word):
     while code < end:
         c = chr(code)
         name = unicodedata.name(c, "*** unknown ***")
-        if word is None or word in name.lower():
+        if word is None or word.issubset(set(name.lower().split(" "))):
             print("{0:7}  {0:5X}  {0:^3c}  {1}".format(
                   code, name.title()))
         code += 1
@@ -35,6 +35,6 @@ if len(sys.argv) > 1:
         print("usage: {0} [string]".format(sys.argv[0]))
         word = 0
     else:
-        word = sys.argv[1].lower()
-if word != 0:
+        word = set(" ".join(sys.argv[1:]).lower().split(" "))
+if word != 0: 
     print_unicode_table(word)
