77e0a54c815620cb936db50a2793c445ab6dc9cf
stemberk <stemberk@spsmb.cz>
2022-09-27 14:33:18 +0200

    -

--- w04/prace_s_kolekcemi.py
+++ w04/prace_s_kolekcemi.py
@@ -12,3 +12,10 @@ random.shuffle(l2)
 print(l1)
 print(l2)
 print(r)
+
+#řazení
+tList = list(zip(r, l1, l2))
+print(tList)
+# chci řadit dle druhého prvku, opužiji pomocnou (zde anonymní) funkci jako
+# klíč, dle kterého se řadí
+print(sorted(tList, key = lambda x:x[1]))
