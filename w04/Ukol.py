503c1dbb8d59bc092554db157982dc211cbe8ba5
stemberk <stemberk@spsmb.cz>
2022-09-27 14:47:22 +0200

    -

--- w04/Ukol.py
+++ w04/Ukol.py
@@ -1,3 +1,11 @@
 # Seřaďte výstup  programu uniquewords2.py dle četnosti (místo abecedního řazení).
 # Použijte metodu sorted(dict, key=funkce), kde funkce je jednoduchá metoda pro
 # extrakci dané hodnoty z prvku (item).
+
+#2.
+# Upravte program generate_usernames.py tak, aby vypisoval údaje dvou 
+# uživatelů na jednom řádku s tím, že jména omezíte na 17 znaků, po
+# každých 64 řádcích vypíšete znak posunu na další stránku 
+# (Form Feed '\f') a na začátku každé stránky se vypíše záhlaví sloupců:
+#Jméno               ID    Už. jm.  Jméno               ID    Už. jm.
+#Woodburn, Farjan.. (4645) fwoodbur Wortham, Erona.... (5422) ewortham
