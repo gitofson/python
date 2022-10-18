c65d8a8f2d49c8dec94526ba8c157f11c1575a6a
stemberk <stemberk@spsmb.cz>
2022-10-18 14:42:36 +0200

    _

---
+++ w06/moduly_a_balicky.py
@@ -0,0 +1,21 @@
+# importování balíčků pomocí příkazu import
+
+filename="moduly_a_balicky.py"
+
+# bezpečný přístup
+import os
+print (os.path.basename(filename)) 
+
+# riziko kolize názvů s path
+import os.path as path
+print (path.basename(filename)) 
+
+# riziko kolize názvů s basename
+from os.path import basename
+print (basename(filename))
+
+# riziko kolize spousty názvů - z daného  modulu importujeme vše
+from os.path import *
+print (basename(filename)) 
+
+# Balíček založíme tak, že vytvoříme adresář a do něj vytvoříme soubor __init__.py
