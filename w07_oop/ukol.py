a518f28419b7eb29ff06cf9387bd6d57c6169ac9
stemberk <stemberk@spsmb.cz>
2022-11-07 14:57:30 +0100

    .\ukol.py

--- w07_oop/ukol.py
+++ w07_oop/ukol.py
@@ -10,3 +10,10 @@
 # p /= n
 # p = q // n
 # p //= n
+
+# Upravte třídu Image (v souboru Image.py) tak, aby poskytovala metodu resize
+# (width, height). Je-li nová šířka menší, než aktuální hodnota, je nutné
+# všechny barvy mimo nové hranice vymazat. Máli jeden z parametrů hodnotu None,
+# použije se stávající výška, resp. šířka. Na konci nezapomeňte vygenerovat
+# množinuself.__colors. Metoda vrátí logickou hodnotu signalizující, zda změny
+# byly, či nebyly provedeny.
