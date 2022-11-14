0b236780304b4fe0ed484b3a06b372f1df608b99
stemberk <stemberk@spsmb.cz>
2022-11-14 11:17:56 +0100

    _

---
+++ w07_oop/e_specialni_metody_kolekci.py
@@ -0,0 +1,7 @@
+# __contains__(self, x)     x in y          Vrátí True pokud se x nachází v kolekci y.
+# __delitem__(self, k)      del y[k]        Vymaže prvek na pozici k posloupnosti y.
+# __getitem__(self, k)      y[k]            Vrací prvek na pozici k z posloupnosti y.
+# __iter__(self)            for x in y: pass    Vrací iterátor pro prvky posl. y.
+# __len__(self)             len(y)          Vrací počet prvků v y.
+# __reversed__(self)        reversed(y)     Vrací zpětný iterátor pro prvky posl. y.
+# __setitem__(self, k, v)   y[k] = v        Nastaví prvek na pozici k posl. y. 
