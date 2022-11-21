1999219dc302b26bef165e1947102933c537227b
stemberk <stemberk@spsmb.cz>
2022-11-21 13:22:44 +0100

    rect+

--- w08_pygame/d_kresleni_obrazku.py
+++ w08_pygame/d_kresleni_obrazku.py
@@ -20,9 +20,15 @@ class App:
     def on_loop(self):
         pass
     def on_render(self):
-        # metoda blit vykreslí obrázek na dané pozici, lze vykreslit i než část pomocí
+        # metoda blit vykreslí do paměti obrázek na dané pozici, lze vykreslit i než část pomocí
         # self.display_surf.blit(self._image_surf,, (0,0) , rect_containing_coordinates_to_draw)
         self._display_surf.blit(self._image_surf,(0,0))
+        color = (255,0,0)
+        # vykreslení plného obdelníku červenou barvou
+        pygame.draw.rect(self._display_surf, color, pygame.Rect(40, 40, 50, 50))
+        # vykreslení obdelníku červenou barvou čárou tloušťky 10
+        pygame.draw.rect(self._display_surf, color, pygame.Rect(100, 40, 50, 50), 10)
+        # Metoda flip zobrazí obrázek z paměti na obrazovku
         pygame.display.flip()
  
     def on_cleanup(self):
