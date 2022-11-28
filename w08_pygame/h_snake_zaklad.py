de8f11394384340459f43d6624c4d0c3f29c86a6
stemberk <stemberk@spsmb.cz>
2022-11-28 13:30:30 +0100

    zadani plus

--- w08_pygame/h_snake_zaklad.py
+++ w08_pygame/h_snake_zaklad.py
@@ -1,4 +1,11 @@
-# 1. vykreslit hada,
+# inspirace: https://zetcode.com/javagames/snake/
+
+# Snake is an older classic video game. 
+# It was first created in late 70s. Later it was brought to PCs. In this game the player controls a snake. 
+# The objective is to eat as many apples as possible. Each time the snake eats an apple its body grows.
+# The snake must avoid the walls and its own body. This game is sometimes called Nibbles.
+
+# 1. Vykreslit hada,
 # 2. animace hada,
 # 3. prodloužední hada při konzumaci jablka,
 # 4. počítadlo score,
