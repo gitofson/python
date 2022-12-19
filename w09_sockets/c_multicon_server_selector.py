e10c6c623888caa6e45e8c462dbe4d375f623678
stemberk <stemberk@spsmb.cz>
2022-12-19 13:57:29 +0100

    _

--- w09_sockets/c_multicon_server_selector.py
+++ w09_sockets/c_multicon_server_selector.py
@@ -1,14 +1,43 @@
 # multiconn-server.py
+# Možnosti pro vícenásobné spojení (více klientů) na server:
+# 1. použití vláken
+# 2. použití asyncio
+# 3. použití třídy Selector z balíčku selectors. Její instance bude registrována
+#    k danému socketu.
 
 import sys
 import socket
 import selectors
 import types
 
-sel = selectors.DefaultSelector()
+def accept_wrapper(sock):
+    conn, addr = sock.accept()  # Should be ready to read
+    print(f"Accepted connection from {addr}")
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data=data)
 
-# ...
-
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)  # Should be ready to read
+        if recv_data:
+            data.outb += recv_data
+        # data nedorazila, což znamená, že klient socket zavřel. Zavřeme ho tedy i na straně serveru
+        else:
+            print(f"Closing connection to {data.addr}")
+            # odstranění z monitoringu selectů
+            sel.unregister(sock)
+            sock.close()
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print(f"Echoing {data.outb!r} to {data.addr}")
+            sent = sock.send(data.outb)  # Should be ready to write
+            data.outb = data.outb[sent:]
+       
+sel = selectors.DefaultSelector()     
 host, port = sys.argv[1], int(sys.argv[2])
 lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 lsock.bind((host, port))
@@ -31,30 +60,3 @@ except KeyboardInterrupt:
     print("Caught keyboard interrupt, exiting")
 finally:
     sel.close()
-
-def accept_wrapper(sock):
-    conn, addr = sock.accept()  # Should be ready to read
-    print(f"Accepted connection from {addr}")
-    conn.setblocking(False)
-    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    sel.register(conn, events, data=data)
-
-def service_connection(key, mask):
-    sock = key.fileobj
-    data = key.data
-    if mask & selectors.EVENT_READ:
-        recv_data = sock.recv(1024)  # Should be ready to read
-        if recv_data:
-            data.outb += recv_data
-            # data nedorazila, což znamená, že klient socket zavřel. Zavřemo ho tedy i na straně serveru
-        else:
-            print(f"Closing connection to {data.addr}")
-            # odstranění z monitoringu selectů
-            sel.unregister(sock)
-            sock.close()
-    if mask & selectors.EVENT_WRITE:
-        if data.outb:
-            print(f"Echoing {data.outb!r} to {data.addr}")
-            sent = sock.send(data.outb)  # Should be ready to write
-            data.outb = data.outb[sent:]
