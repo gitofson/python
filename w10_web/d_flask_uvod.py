82c37ec56859e812e8fca2ee9eddc9b6acd4a3ce
stemb <stemberk@spsmb.cz>
2023-02-06 13:12:04 +0100

    _

--- w10_web/d_flask_uvod.py
+++ w10_web/d_flask_uvod.py
@@ -1,10 +1,9 @@
 
-# Flask - mikrowebový framework, neobsahuje abstrakce pro DB, další vnitřní knihovny ani žádné jiné komponenty třetích stran.
+# Flask - mikrowebový framework, neobsahuje projektový inicializátor, abstrakce pro DB, další vnitřní knihovny ani žádné jiné komponenty třetích stran.
 #         Používá šablonovací systém Jinja, který obdobně jako Django využívá pro přístup k proměnným {{ }} a pro příkazy {% %} 
 # tutorial:
 # https://www.tutorialspoint.com/flask/
 # instalace: 
 # py.exe -m pip install -U flask --user
 # 
-# inicializace projektu:
 # 
