4a6d2ba1b0a73052ca162b9ea60301ad78929d8e
stemb <stemberk@spsmb.cz>
2023-02-06 09:29:14 +0100

    flask+

---
+++ w10_web/c_flask_uvod.py
@@ -0,0 +1,10 @@
+
+# flask -
+# tutorial:
+# https://www.tutorialspoint.com/flask/
+# případně: https://flask.palletsprojects.com/en/2.2.x/tutorial/
+# instalace: 
+# py.exe -m pip install -U flask --user
+# 
+# inicializace projektu:
+# 
---
+++ w10_web/d_hello.py
@@ -0,0 +1,11 @@
+# popis:
+# https://www.tutorialspoint.com/flask/flask_application.htm
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+   return 'Hello World'
+
+if __name__ == '__main__':
+   app.run()
