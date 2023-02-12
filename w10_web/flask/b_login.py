e76633662cecb69d96caf3758f52060573c59371
stemb <stemberk@spsmb.cz>
2023-02-12 18:54:09 +0100

    _

---
+++ w10_web/flask/b_login.py
@@ -0,0 +1,22 @@
+from flask import Flask, redirect, url_for, request, render_template
+app = Flask(__name__)
+
+@app.route('/success/<name>')
+def success(name):
+   return 'welcome %s' % name
+
+@app.route('/login')
+def login():
+   return render_template('login.html')
+
+@app.route('/lgn',methods = ['POST', 'GET'])
+def lgn():
+   if request.method == 'POST':
+      user = request.form['nm']
+      return redirect(url_for('success',name = user))
+   else:
+      user = request.args.get('nm')
+      return redirect(url_for('success',name = user))
+
+if __name__ == '__main__':
+   app.run(debug = True)
