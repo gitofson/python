adb8ac1b1f3b5deda1b110e8e17f00d146a8844f
stemberk <stemberk@spsmb.cz>
2023-02-20 13:58:17 +0100

    _

---
+++ w11_sem/b_zadani.py
@@ -0,0 +1,15 @@
+# Abraham Christian	        Django, Album							 		 	
+# Demjan Mattias Justin
+
+# Kočí František            Pygame, tanky           https://github.com/KociFanda/TankPython
+# Dva tanky, každý tank ovládá jeden hráč# #Snaží se navzájem zastřelit 
+# #Kolize tanků, střel a krajů obrazovky #Životy tanků #Oživování tanků #Skore
+
+# Krouský Robin
+# Novák Filip               Pygame
+# Novotný Jakub
+# Poláček Jan
+# Reindl Erik               Pygame
+# Schön Otto	            
+# Vít Vojtěch                                       https://github.com/vojtechos/PythonProject
+# Zikmund Radek             
