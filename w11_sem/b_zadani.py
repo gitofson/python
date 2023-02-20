207e1c4a71119184eac361a87d70e0fc86dac81c
stemberk <stemberk@spsmb.cz>
2023-02-20 15:10:24 +0100

    _

--- w11_sem/b_zadani.py
+++ w11_sem/b_zadani.py
@@ -1,15 +1,19 @@
-# Abraham Christian	        Django, Album							 		 	
-# Demjan Mattias Justin
+# Abraham Christian	        Django, Album	            https://github.com/Chr1st1ancz/pajtnprojekt						 		 	
+# Demjan Mattias Justin     Flask, Babiččiny recepty    https://github.com/MattiasJD/piton-seminarni-arbeit
 
-# Kočí František            Pygame, tanky           https://github.com/KociFanda/TankPython
+# Kočí František            Pygame, tanky               https://github.com/KociFanda/TankPython
 # Dva tanky, každý tank ovládá jeden hráč# #Snaží se navzájem zastřelit 
 # #Kolize tanků, střel a krajů obrazovky #Životy tanků #Oživování tanků #Skore
 
-# Krouský Robin
-# Novák Filip               Pygame
-# Novotný Jakub
-# Poláček Jan
-# Reindl Erik               Pygame
-# Schön Otto	            
-# Vít Vojtěch                                       https://github.com/vojtechos/PythonProject
+# Krouský Robin             Pygame, Dyno game           
+# Novák Filip               Textové RPG                 https://github.com/filipusnovakus/PythonRPG
+
+# Novotný Jakub             Pygame, Minesweeper         
+# obdoba https://zetcode.com/javagames/minesweeper/
+
+# Poláček Jan               
+# Reindl Erik               Pygame,                     https://github.com/KRBNJSF/PythonGame
+
+# Schön Otto	            Pygame, shoothemup          https://github.com/OtanCZ/shootemup-pygame
+# Vít Vojtěch               Pygame, tetris              https://github.com/vojtechos/PythonProject
 # Zikmund Radek             
