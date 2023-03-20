a5f51a7fe7240a5938fafb4eae9a4a625f322486
stemberk <stemberk@spsmb.cz>
2023-03-20 15:30:18 +0100

    _

--- w11_sem/b_zadani.py
+++ w11_sem/b_zadani.py
@@ -1,4 +1,9 @@
-# Abraham Christian	        Django, Album	            https://github.com/Chr1st1ancz/pajtnprojekt						 		 	
+# Abraham Christian	        Django, Album	            https://github.com/Chr1st1ancz/pajtnprojekt
+# Dodělat: 
+#   Výběr váce souborů,
+#   miniatury všech fotek na hlavní stránce, nejlépe ve stjné velikosti,
+#   lepší dekorace pomocí fontů.
+						 		 	
 # Demjan Mattias Justin     Flask, Babiččiny recepty    https://github.com/MattiasJD/piton-seminarni-arbeit
 
 # Kočí František            Pygame, tanky               https://github.com/KociFanda/TankPython
@@ -7,12 +12,27 @@
 
 # Krouský Robin             Pygame, Dyno game           
 # Novák Filip               Textové RPG                 https://github.com/filipusnovakus/PythonRPG
+# dokončit 
+#  shop, 
+#  zpomalit samotný boj (napínavější hra, možnost použití healing potion),
+#  rozdrobit kód do vícero metod,
+#  použít oop (hlavně pro možnost více stejných příšer na různých místech),
+#  přidat mapu.
 
 # Novotný Jakub             Pygame, Minesweeper         
 # obdoba https://zetcode.com/javagames/minesweeper/
 
 # Poláček Jan               Pygame, skateboard gamme    https://github.com/neostetic/python-skater-game
-# Reindl Erik               Pygame,                     https://github.com/KRBNJSF/PythonGame
+# Dodělat
+#    omezený pohyb hráče
+#    přidat překážky,
+#    přidat sbírání mincí
+
+# Reindl Erik               Pygame, stone pickup        https://github.com/KRBNJSF/PythonGame
+# dodělat
+# pozadí
+# hudba,
+# zvuky
 
 # Schön Otto	            Pygame, shoothemup          https://github.com/OtanCZ/shootemup-pygame
 # Vít Vojtěch               Pygame, tetris              https://github.com/vojtechos/PythonProject
