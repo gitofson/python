1d55dbd60e5f5da50333fecc2e60467a7261c1ed
stemberk <stemberk@spsmb.cz>
2023-03-21 15:29:07 +0100

    _

--- w11_sem/a_zadani.py
+++ w11_sem/a_zadani.py
@@ -1,16 +1,40 @@
 # Bílek Daniel	        Tkinter, chatovací aplikace         https://github.com/Daniel2B9/Chat-app-python-tkinter
+# dodělat:
+# jména uživatelů, textové okno zprávy, resp. historii zpráv. Zamyslet se nad libovolnou délkou zprávy. 
+# Funkčnost více klientů (použítí selektorů)
+
 # Fiala Marek           Django, správa knihovny             https://github.com/marekfiala/python_knihovna
+# dodělat:
+# kategorie knih, detaily kategorií
+
+
 # Hofman Jan            Pygame, Flappy Bird                 https://github.com/hofmic92/FlappyBirdPy
+# dodělat :
+# plynulý pohyb ptáčka pomocí kláves
+# použít OOP 
+
 # Hojný Martin          Pygame, šibenice                    https://github.com/AntiIllegal/python-game
 # Jiránek Jiří          Textové RPG                         https://github.com/JirkaJiry/python_text_rpg 
 # Kargl Tomáš           Tkinter, Tic Tac Toe                https://github.com/kargltomas/Tic-tac-toe
+# dodělat:
+# funkčnost (střídání hráčů), OOP
+
 # Novák Adam            Pygame, rock paperr scissors        https://github.com/Buftik/PythonProj
+# dodělat:
+# používat OOP, vizualizace pomocí pygame, 
+
 # Novák Štěpán          Pygame, Breakout                    https://github.com/SH4RK7/Breakout
+
 # Odstrčil Vojtěch      Tkinter, kalkulátor                 https://github.com/Vojtech5/pythonThing
+# dodělat GUI
+
 # Škrabánek Lukáš       Pygame, tetris                      https://github.com/Raysenn/TetrisGame
 # inspirace: https://zetcode.com/javagames/tetris/
 
 # Tondl Tadeáš          Pygame, Pong                        https://github.com/tadeastondl/Pygame-Pong                  
 # Vítek Jan             Flask, Weather Focus                https://github.com/Hanis03/py_project_4
+# dodělat:
+# grafické zobrazení hodnot
+
 # Zralý Matěj           Pygame, Space Invaders              https://github.com/matejzraly/Space-Invaders
 # inspirace: https://zetcode.com/javagames/spaceinvaders/
