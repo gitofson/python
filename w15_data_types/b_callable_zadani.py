d7e20b3518791ea1bca33a471f7bb5e28a34c3fb
stemberk <stemberk@spsmb.cz>
2023-04-11 14:24:42 +0200

    _

---
+++ w15_data_types/b_callable_zadani.py
@@ -0,0 +1,4 @@
+# Nadefinujte si 4 metody (sčítání, odečítání, násobení a dělení) dvou čísel typu
+# float.
+# Pomocí třídy Callable proveďte vlastní operaci na základě náhodného čísla.
+# U všeho uveďte typ.
